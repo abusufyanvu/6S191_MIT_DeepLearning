2c4966842d8efceeab2916b49d57e8177f6fb2c3
Abu Sufyan <76678681+thisisabusufyan@users.noreply.github.com>
2022-01-16 16:22:48 +0500

    Add files via upload

---
+++ 6S191_MIT_DeepLearning/introtodeeplearning-labs/lab1/README.md
@@ -0,0 +1,9 @@
+# MIT 6.S191 Lab 1: Intro to TensorFlow and Music Generation with RNNs
+
+![alt text](https://github.com/aamini/introtodeeplearning/raw/master/lab1/img/music_waveform.png)
+## Part 1: Intro to TensorFlow
+TensorFlow is a software library extensively used in machine learning. Here we'll learn how computations are represented and how to define simple neural networks in TensorFlow. TensorFlow uses a high-level API called [Keras](https://www.tensorflow.org/guide/keras) that provides a powerful, intuitive framework for building and training deep learning models. In this section you will learn the basics of computations in TensorFlow, the Keras API, and TensorFlow's new imperative execution style enabled by [Eager](https://research.googleblog.com/2017/10/eager-execution-imperative-define-by.html).
+
+## Part 2: Music Generation with RNNs
+In the second portion of the lab, we will play around with building a Recurrent Neural Network (RNN) for music generation. We will be using a "character RNN" to predict the next character of sheet music in ABC notation. Finally, we will sample from this model to generate a brand new music file that has never been heard before!
+
---
+++ 6S191_MIT_DeepLearning/introtodeeplearning-labs/mitdeeplearning/util.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import time
+import numpy as np
+
+from IPython import display as ipythondisplay
+from string import Formatter
+
+
+
+
+
+def display_model(model):
+  tf.keras.utils.plot_model(model,
+             to_file='tmp.png',
+             show_shapes=True)
+  return ipythondisplay.Image('tmp.png')
+
+
+def plot_sample(x,y,vae):
+    plt.figure(figsize=(2,1))
+    plt.subplot(1, 2, 1)
+
+    idx = np.where(y==1)[0][0]
+    plt.imshow(x[idx])
+    plt.grid(False)
+
+    plt.subplot(1, 2, 2)
+    _, _, _, recon = vae(x)
+    recon = np.clip(recon, 0, 1)
+    plt.imshow(recon[idx])
+    plt.grid(False)
+
+    plt.show()
+
+
+
+class LossHistory:
+  def __init__(self, smoothing_factor=0.0):
+    self.alpha = smoothing_factor
+    self.loss = []
+  def append(self, value):
+    self.loss.append( self.alpha*self.loss[-1] + (1-self.alpha)*value if len(self.loss)>0 else value )
+  def get(self):
+    return self.loss
+
+class PeriodicPlotter:
+  def __init__(self, sec, xlabel='', ylabel='', scale=None):
+
+    self.xlabel = xlabel
+    self.ylabel = ylabel
+    self.sec = sec
+    self.scale = scale
+
+    self.tic = time.time()
+
+  def plot(self, data):
+    if time.time() - self.tic > self.sec:
+      plt.cla()
+
+      if self.scale is None:
+        plt.plot(data)
+      elif self.scale == 'semilogx':
+        plt.semilogx(data)
+      elif self.scale == 'semilogy':
+        plt.semilogy(data)
+      elif self.scale == 'loglog':
+        plt.loglog(data)
+      else:
+        raise ValueError("unrecognized parameter scale {}".format(self.scale))
+
+      plt.xlabel(self.xlabel); plt.ylabel(self.ylabel)
+      ipythondisplay.clear_output(wait=True)
+      ipythondisplay.display(plt.gcf())
+
+      self.tic = time.time()
